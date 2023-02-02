0d14d64f24c24b2a19fa2c1dcf368eb29c19791d
yecksin <yecksin@gmail.com>
2023-02-02 10:02:56 -0500

    :sparkles: feat(app component): routing developed

--- prms-bi-client/src/app/app-routing.module.ts
+++ prms-bi-client/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BiModule } from './pages/bi/bi.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'bi/:id',
+    loadChildren: () => import('./pages/bi/bi.module').then((m) => m.BiModule),
+  },
+  // { path: '**', pathMatch: 'full', redirectTo: 'bi' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
