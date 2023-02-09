41bb3909a3cfc1f3b000c4d484537ace36666ddd
yecksin <yecksin@gmail.com>
2023-02-09 14:52:33 -0500

    :sparkles: feat(export-tables): developed

--- prms-bi-client/src/app/services/bi-implementation.service.ts
+++ prms-bi-client/src/app/services/bi-implementation.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import * as pbi from 'powerbi-client';
+import { ExportTablesService } from './export-tables.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BiImplementationService {
-  constructor(public http: HttpClient) {}
+  constructor(
+    public http: HttpClient,
+    private exportTablesSE: ExportTablesService
+  ) {}
 
   apiBaseUrl = environment.apiBaseUrl + 'result-dashboard-bi';
   getBiReportsWithCredentials() {
@@ -35,7 +39,7 @@ export class BiImplementationService {
       // pageView: 'fitToWidth',
       settings: {
         filterPaneEnabled: false,
-        navContentPaneEnabled: true,
+        navContentPaneEnabled: false,
       },
     };
     var embedContainer: any = document.getElementById('reportContainer');
@@ -52,5 +56,67 @@ export class BiImplementationService {
     report.on('error', () => {
       console.log('ERROR WEY');
     });
+    this.exportButton(report);
+  }
+
+  exportButton(report: any) {
+    // Insert here the code you want to run after the report is rendered
+
+    // report.off removes all event handlers for a specific event
+
+    report.off('bookmarkApplied');
+
+    // report.on will add an event listener.
+
+    report.on('bookmarkApplied', async (event: any) => {
+      if (event.detail.bookmarkName == 'Bookmarkc25052bc6f133cc544c3') {
+        console.log('Exporting data...\n');
+
+        try {
+          const pages = await report.getPages();
+          let page = pages.filter((page: any) => {
+            return page.isActive;
+          })[0];
+
+          const visuals = await page.getVisuals();
+          let visual = visuals.filter((visual: any) => {
+            return visual.name === '19eaa2a666788114ee07';
+          })[0];
+
+          const result = await visual.exportData(
+            pbi.models.ExportDataType.Summarized
+          );
+
+          this.dataToObject(result.data);
+        } catch (errors) {
+          console.log(errors);
+        }
+      }
+    });
+  }
+
+  dataToObject(data: any) {
+    // console.log(data);
+    let list: any[] = [];
+    let header = data.split('\r\n')[0].split(',');
+    // console.log(header);
+
+    let allRows = data.split('\r\n').map((dataItem: any) => {
+      let dataEx: any = {};
+
+      let dataSplited = dataItem.replace(
+        /(,)(?=(?:[^"]|"[^"]*")*$)/g,
+        '|coma|'
+      );
+      dataSplited = dataSplited.split('|coma|');
+      console.log(dataSplited);
+      header.map((head: any, index: any) => {
+        dataEx[' ' + head] = dataSplited[index];
+      });
+      list.push(dataEx);
+    });
+
+    console.log(list);
+    this.exportTablesSE.exportExcel(list, 'Results detail');
   }
 }
---
+++ prms-bi-client/src/app/services/export-tables.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import * as FileSaver from 'file-saver';
+interface Wscols {
+  wpx: number;
+}
+@Injectable({
+  providedIn: 'root',
+})
+export class ExportTablesService {
+  constructor() {}
+  exportExcel(list: any, fileName: string, wscols?: Wscols[]) {
+    import('xlsx').then((xlsx) => {
+      const worksheet = xlsx.utils.json_to_sheet(list, {
+        skipHeader: true,
+      });
+      if (wscols) worksheet['!cols'] = wscols as any;
+      const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
+      const excelBuffer: any = xlsx.write(workbook, {
+        bookType: 'xlsx',
+        type: 'array',
+      });
+      this.saveAsExcelFile(excelBuffer, fileName);
+    });
+  }
+  private saveAsExcelFile(buffer: any, fileName: string): void {
+    let EXCEL_TYPE =
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    let EXCEL_EXTENSION = '.xlsx';
+    const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE,
+    });
+    FileSaver.saveAs(
+      data,
+      fileName + '_' + new Date().getTime() + EXCEL_EXTENSION
+    );
+  }
+}
